54fa8986675f5fb4c36cbafacda07262d8e8ecff
WilliamCoulter <wwcoulter6@gmail.com>
2022-01-18 11:31:42 -0800

    Deleted AppHelperFunctions folder. Helper functions are now inside app designer

--- AppHelperFunctions/PlotSPDs.m
+++
@@ -1,6 +0,0 @@
-%% Note that this is not a function. It just wraps a lot of commands into one name
-
-%% Plot Data
-plot(app.UIAxes_ImportedSPDs, 380:wlIntUser:780, app.userSPDs_Prop(:,1:end));
-app.UIAxes_ImportedSPDs.XLim = [380,780];
-
