8e89442a031d85da76af1491379d8045d7095842
WilliamCoulter <wwcoulter6@gmail.com>
2022-01-19 12:51:41 -0800

    Practiced making c and ceq vectors via the uiTable. Added the process as a loop for readability into "uit_constraintsToConstraintVectors"

--- Debugging/practiceUITableToConstraint.m
+++ Debugging/practiceUITableToConstraint.m
@@ -8,8 +8,40 @@ load('C:\Users\Will\OneDrive\MyMatlab\MetaMetric\Debugging\dummyUITable.mat')
 uit = t;
 
 t = uit.Data
-
+t.UseTF
 %% Think through how to pull data from table
 % How many rows in the constraint vectors c, ceq?
 % Metric is used only if UseTF == true, and when it is true, there are at
 % most 3 per metric for each of the three equality types.
+
+%% Let's just use a loop to make it easier to read
+% Loop each row (metric) and proceed to next loop if it is used (UseTF)
+% Then go through each equality type and output to c, ceq
+c=[];
+ceq=[];
+SpdStruct = spdToTM30(ones(401,1));
+SpdStruct = spdToAlphaOpics(SpdStruct)
+for iMetric = 1:height(t) %go through each row (metric name)
+    if t.UseTF(iMetric) == 1 %if the metric is used AT ALL
+
+        currentVal = SpdStruct.(t.Metrics{iMetric}); %Current spd value
+        
+        %% go through each equality types
+        if t.EqualToTF(iMetric) == 1 %if we want to use equal to
+            constraintVal = t.EqualToVal(iMetric); %value user set to be equal to
+            ceq(end+1,1) = currentVal - constraintVal;
+        end
+        if t.LessThanTF(iMetric) == 1 %if we want to use less than
+            constraintVal = t.LessThanVal(iMetric);
+            c(end+1,1) = currentVal - constraintVal;
+        end
+        if t.GreaterThanTF(iMetric) == 1 %if we want to use greater than
+            constraintVal = t.GreaterThanVal(iMetric);
+            c(end+1,1) = -1* [ currentVal - constraintVal];
+        end
+    end
+end
+
+
+
+
