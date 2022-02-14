ba49a3fa8140e6311f6127bcd3ef9e4192e7179c
WilliamCoulter <wwcoulter6@gmail.com>
2022-02-14 13:04:58 -0800

    deleted "metricResultsStructToTable" from app function methods and remade a function under functions > app_methods with same name and verified it still works.

---
+++ Functions/App_Methods/metricResultsStructToTable.m
@@ -0,0 +1,17 @@
+function metricResultsStructToTable(app)
+%METRICRESULTSSTRUCTTOTABLE Summary of this function goes here
+%   Detailed explanation goes here
+% trim structure to get metrics in a column
+% I practiced this in "practiceSaveResultsToFile.mlx"
+% List out which metrics to delete.
+metricDeleteList = {'sref','stest','rfBins','hsBins','csBins',...
+    'gref','gtest','SpdPercents0','Solution','wl'};
+% Create scalar struct (size 1) that will be output to file by
+% deleting fields listed above
+myStructForTable = rmfield(app.myOptimResults.metrics,metricDeleteList);
+
+app.metricResultsTable = struct2table(myStructForTable,'AsArray',true);
+app.metricResultsTable = rows2vars(app.metricResultsTable);
+app.metricResultsTable.Properties.VariableNames = ["Metrics", "Value of Optimized SPD"];
+end
+
