b6bce4eb498e47bcaf2f1b62261a843512e92513
WilliamCoulter <wwcoulter6@gmail.com>
2022-02-14 12:58:37 -0800

    Deleted "makeUIConstraintTable" as method and created a function of the same name that takes app as the sole input argument.

---
+++ Functions/App_Methods/makeUIConstraintTable.m
@@ -0,0 +1,54 @@
+function makeUIConstraintTable(app)
+
+%% Replicate metric generation done in optimziation function
+dummyStruct = spdToTM30(ones(401,1)); %just used to get fields
+dummyStruct = spdToAlphaOpics(dummyStruct);
+%% Clumsily shuffle the bin fields to the end
+% The 48 bin fields are assigned programmatically using a
+% clever trick, but it can only be done to create a structure,
+% not add to an existing one. A workaround could make spdToTM30
+% run significantly more slowly.
+dummyStruct = shuffleStructFields(dummyStruct,51);
+dummyStruct = shuffleStructFields(dummyStruct,19);
+%% Get the fieldnames, which are the metrics. These are stored in app properties
+app.MetricsEvaluated = fieldnames(dummyStruct);
+%% Create columns based on the number of elements
+% Set default constraints to none being used
+UseTF         = false( numel(app.MetricsEvaluated),1);
+LessThanTF    = false(numel(app.MetricsEvaluated),1);
+EqualToTF     = true(numel(app.MetricsEvaluated),1); %default
+GreaterThanTF = false(numel(app.MetricsEvaluated),1 );
+
+% Min > -inf, max < +inf, and equal is default to 0.
+LessThanVal    = 1*inf*ones( numel(app.MetricsEvaluated), 1);
+EqualToVal     = zeros(numel(app.MetricsEvaluated),1);
+GreaterThanVal = -1*inf*ones( numel(app.MetricsEvaluated),1);
+
+%% Create a standard table
+% use comma-separated variables as variablenames
+myConstraintTableNames = ["Metric.","Use This Metric At All?",...
+    "Use < Constraint?", "Less than what?",...
+    "Use = Constraint?", "Equal to what?",...
+    "Use > Constraint?", "Greater than what?"];
+
+constraints_Table = table(app.MetricsEvaluated, UseTF,...
+    LessThanTF,LessThanVal,...
+    EqualToTF, EqualToVal,...
+    GreaterThanTF,GreaterThanVal,...
+    VariableNames=myConstraintTableNames);
+
+
+%% Fill in the uitable
+app.UITable_Constraints.Data = constraints_Table;
+%% Set utable properties
+app.UITable_Constraints.ColumnEditable = true;
+app.UITable_Constraints.Multiselect = true;
+%             app.UITable_Constraints.ColumnWidth = 'fit';
+app.UITable_Constraints.FontSize = 18;
+app.UITable_Constraints.RowStriping ='on';
+%% Add style
+styleCentered = uistyle('HorizontalAlignment','center');
+addStyle(app.UITable_Constraints, styleCentered, 'column', [2:width(app.UITable_Constraints.Data)] );
+
+end
+
