b58ad88447d897737fd63b811afb07f54f75dd7f
WilliamCoulter <wwcoulter6@gmail.com>
2022-02-23 15:20:56 -0800

    Realizing that I want a list of all possible metrics and to then select from that, and that doing so is a complex parsing code, I made a "getAllPossibleMetrics" function to help out.
    This can be called on startup to set a global app property.
    
    I realized I should have this when I saw that the previous method of doing this was going to have the optimization goal only be possible based on constraints being checked

--- Functions/App_Methods/makeUIConstraintTable.m
+++ Functions/App_Methods/makeUIConstraintTable.m
@@ -1,43 +1,44 @@
 function [MetricsEvaluated]= makeUIConstraintTable(app)
 
 %% Replicate metric generation done in optimziation function
-dummyStruct = channelPercentsToSPDNestedStruct(ones(401,1));
-%
-fn = fieldnames(dummyStruct);
-for fnIdx = 1:numel(fn)
-    [dummyStruct.(fn{fnIdx})] = removeNonScalarFields(dummyStruct.(fn{fnIdx}) );
-end
-
-nestStructNames = fieldnames(dummyStruct); %these are the nested structure
-allMetrics = {};
-for nestStructIdx = 1:numel(nestStructNames) %first fields are always structs. go through each
-    
-    nestStruct = dummyStruct.(nestStructNames{nestStructIdx}); %first layer always struct. temporarily assign
-    if ~isstruct(nestStruct)
-        error("Fieldnames of struct must all be structures for program - Will C")
-    end
-    % Go through each nested struct. Remember each nested struct is a
-    % category like TM30, Power, AOpics
-    fn = fieldnames(nestStruct);
-    for fnIdx = 1:numel(fn) %go through the fields of the nested struct
-        if isstruct( nestStruct.(fn{fnIdx}) )
-            anyStructTF = all(structfun(@isstruct,nestStruct.(fn{fnIdx}) ) );
-            anyNonScalarTF = ~all(structfun(@isscalar,nestStruct.(fn{fnIdx}) ) );
-            if anyStructTF || anyNonScalarTF %if a structure is found or they are not all scalar
-                error("Third layer of your nested structure can contain not structures..." + ...
-                    "Why would you want to make this even more complicated :D -Will C")
-            end
-            allMetrics = [allMetrics; fieldnames(nestStruct.(fn{fnIdx}) )];
-        elseif ~isstruct(nestStruct.(fn{fnIdx})) && isscalar(nestStruct.(fn{fnIdx}))
-            allMetrics = [allMetrics;  fn{fnIdx}] %if fieldname is not a struct and is scalar, add it
-        elseif ~isscalar(nestStruct.(fn{fnIdx}))
-            error("The fields need to be nonscalar for the program. ..." + ...
-                "They should have been removed via my removeNonScalarFields function -Will C")
-        end
-    end
-end
+% dummyStruct = channelPercentsToSPDNestedStruct(ones(401,1));
+% %
+% fn = fieldnames(dummyStruct);
+% for fnIdx = 1:numel(fn)
+%     [dummyStruct.(fn{fnIdx})] = removeNonScalarFields(dummyStruct.(fn{fnIdx}) );
+% end
+% 
+% nestStructNames = fieldnames(dummyStruct); %these are the nested structure
+% allMetrics = {};
+% for nestStructIdx = 1:numel(nestStructNames) %first fields are always structs. go through each
+%     
+%     nestStruct = dummyStruct.(nestStructNames{nestStructIdx}); %first layer always struct. temporarily assign
+%     if ~isstruct(nestStruct)
+%         error("Fieldnames of struct must all be structures for program - Will C")
+%     end
+%     % Go through each nested struct. Remember each nested struct is a
+%     % category like TM30, Power, AOpics
+%     fn = fieldnames(nestStruct);
+%     for fnIdx = 1:numel(fn) %go through the fields of the nested struct
+%         if isstruct( nestStruct.(fn{fnIdx}) )
+%             anyStructTF = all(structfun(@isstruct,nestStruct.(fn{fnIdx}) ) );
+%             anyNonScalarTF = ~all(structfun(@isscalar,nestStruct.(fn{fnIdx}) ) );
+%             if anyStructTF || anyNonScalarTF %if a structure is found or they are not all scalar
+%                 error("Third layer of your nested structure can contain not structures..." + ...
+%                     "Why would you want to make this even more complicated :D -Will C")
+%             end
+%             allMetrics = [allMetrics; fieldnames(nestStruct.(fn{fnIdx}) )];
+%         elseif ~isstruct(nestStruct.(fn{fnIdx})) && isscalar(nestStruct.(fn{fnIdx}))
+%             allMetrics = [allMetrics;  fn{fnIdx}] %if fieldname is not a struct and is scalar, add it
+%         elseif ~isscalar(nestStruct.(fn{fnIdx}))
+%             error("The fields need to be nonscalar for the program. ..." + ...
+%                 "They should have been removed via my removeNonScalarFields function -Will C")
+%         end
+%     end
+% end
 % allMetrics = fieldnames(dummyStruct);
-MetricsEvaluated = allMetrics;
+% MetricsEvaluated = allMetrics;
+MetricsEvaluated = app.MetricsEvaluated; %this is updated whenever uitree check is changed
 %% Create columns based on the number of elements
 % Set default constraints to none being used
 UseTF         = false( numel(MetricsEvaluated),1);
---
+++ Functions/getAllPossibleMetrics.m
@@ -0,0 +1,58 @@
+function [allMetrics] = getAllPossibleMetrics
+%% This calls the channelPercentsToSPDNestedStruct and removeNonScalarFields 
+% and parses it to find all metrics that are scalar
+
+%% Create the dummyStruct.
+% Note that some fields are not used for constraints but for other things
+% like plotting, so we need to remove the nonscalar fields.
+% 
+dummyStruct = channelPercentsToSPDNestedStruct(ones(401,1));
+%
+fn = fieldnames(dummyStruct);
+for fnIdx = 1:numel(fn)
+    if ~isstruct(dummyStruct.(fn{fnIdx})) %check that first layer is all struct
+        error("You need to have the first layer of the spd structure that..." + ...
+            "has all of the metrics to be structures themselves");
+    end
+    [dummyStruct.(fn{fnIdx})] = removeNonScalarFields(dummyStruct.(fn{fnIdx}) );
+end
+
+%% Go through the structure and parse out all scalar fieldnames to get a list of all
+% potential metrics
+
+nestStructNames = fieldnames(dummyStruct); %these are the nested structure
+allMetrics = {};
+for nestStructIdx = 1:numel(nestStructNames) %first fields are always structs. go through each
+    
+    nestStruct = dummyStruct.(nestStructNames{nestStructIdx}); %first layer always struct. temporarily assign
+    if ~isstruct(nestStruct) %Like above, ensure they are all struct
+        error("Fieldnames of struct must all be structures for program - Will C")
+    end
+    % Go through each nested struct. Remember each nested struct is a
+    % category like TM30, Power, AOpics
+    fn = fieldnames(nestStruct); %get field names of each nestStruct
+
+    for fnIdx = 1:numel(fn) %go through the fields of the nested struct
+
+        if isstruct( nestStruct.(fn{fnIdx}) )
+            %% Ensure that these are all scalar and nonstructs
+            anyStructTF = all(structfun(@isstruct,nestStruct.(fn{fnIdx}) ) );
+            anyNonScalarTF = ~all(structfun(@isscalar,nestStruct.(fn{fnIdx}) ) );
+            if anyStructTF || anyNonScalarTF %if a structure is found or they are not all scalar
+                error("Third layer of your nested structure can contain not structures..." + ...
+                    "Why would you want to make this even more complicated :D -Will C")
+            end
+            %% 
+            allMetrics = [allMetrics; fieldnames(nestStruct.(fn{fnIdx}) )];
+        elseif ~isstruct(nestStruct.(fn{fnIdx})) && isscalar(nestStruct.(fn{fnIdx}))
+            allMetrics = [allMetrics;  fn{fnIdx}] %if fieldname is not a struct and is scalar, add it
+            
+        elseif ~isscalar(nestStruct.(fn{fnIdx})) % if it is not scalar, like a matrix, then error
+            error("The fields need to be nonscalar for the program. ..." + ...
+                "They should have been removed via my removeNonScalarFields function -Will C")
+        end
+    end
+end
+
+end
+
