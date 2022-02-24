741423c8fcfc1793965e9559712dda2d8588ab20
WilliamCoulter <wwcoulter6@gmail.com>
2022-02-24 12:42:13 -0800

    I was encoutnering issues because the wrapper function called another wrapper function. Remember!!!
    A wrapper function has ALL of the stuff that the callback WOULD have.
    
    -Still  not entirely sure why a wrapper of a wrapper would not continue to pass the app down, but the methods could not be accessed. Doesn't matter though, since I was adding an unneeded layer

--- Functions/App_Wrapper_Functions/wrapper_Tree_ConstraintsCheckedNodesChanged.m
+++ Functions/App_Wrapper_Functions/wrapper_Tree_ConstraintsCheckedNodesChanged.m
@@ -1,7 +1,18 @@
-function [outputArg1,outputArg2] = wrapper_Tree_ConstraintsCheckedNodesChanged(inputArg1,inputArg2)
+function wrapper_Tree_ConstraintsCheckedNodesChanged(app,event)
 %WRAPPER_TREE_CONSTRAINTSCECKEDNODESCHANGED Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+checkedNodes = app.Tree_Constraints.CheckedNodes;
+if ~isempty(checkedNodes) %we can't dot index the tag if it is empty
+    % remove the parent nodes that are checked
+    myCheckedCellArray = {checkedNodes.NodeData};
+    isNodeStructTF = cellfun( @isstruct, myCheckedCellArray);
+    nodesToUse = checkedNodes(~isNodeStructTF); %get all nodes that are not struct
+    metricsToList = {nodesToUse.Tag}';
+    %             app.MetricsEvaluated = {checkedNodes.Tag}';
+    makeUIConstraintTable(app,metricsToList); %remake table
+elseif isempty(checkedNodes)
+    app.UITable_Constraints.Data = [];
+
+end
 end
 
