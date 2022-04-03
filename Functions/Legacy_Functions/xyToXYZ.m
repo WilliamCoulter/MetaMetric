f69b285cd952a47e9b8e1317aa1d9f83b3c79fc9
WilliamCoulter <wwcoulter6@gmail.com>
2022-04-03 10:01:58 -0700

    Remove legacy and library subfolders of Functions folder. The previous removals did not delete. Sometimes it seems to delete instead of remove. Previous removals removed while in the "All" tab of the project GUI. These removals are from the "Project" tab

--- Functions/Legacy_Functions/xyToXYZ.m
+++
@@ -1,9 +0,0 @@
-function [XYZ] = xyToXYZ(xy, lumen)
-%% [XYZ] = xy2XYZ( xy, lumen) where xy is a 2 length vector
-
-%% asume xy is nx2 vector
-z = ones(length(xy(:,1)),1)- xy(:,1) -xy(:,2)
-xyz =  [ xy(:,1), xy(:,2), z]
-for i=1:size(xyz,1)
-    XYZ(:,i) = (lumen./xyz(i,2)).*[ xyz(i,:)]
-end
